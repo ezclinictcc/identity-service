195a96ef0ea4022948081bba9da20e58bd0a5fba
dennisw.dev <dennisw.dev@gmail.com>
2022-08-22 23:02:02 -0300

    Adicionando base de dados do heroku

--- src/adapters/repository/typeorm/data-source.ts
+++ src/adapters/repository/typeorm/data-source.ts
@@ -9,6 +9,28 @@ import { CreateUserType1660229382984 } from "./migrations/user_type/166022938298
 
 const AppDataSource = new DataSource({
   type: "postgres",
+  host: "ec2-44-205-63-142.compute-1.amazonaws.com",
+  port: 5432,
+  username: "drrbrkxgybpncw",
+  password: "f03500e1a45eaa0346040f0e718e65ef62611734af2f41fd66c58789740d4c45",
+  database: "d6rltr94i69giv",
+  synchronize: true,
+  logging: true,
+  entities: ["./src/entities/**/*.ts"],
+  subscribers: [],
+  migrations: [
+    CreateUser1659896492936,
+    CreateProfile1660229516240,
+    CreateUserType1660229382984,
+    CreateClinic1660229536172,
+    V1Release1660256966234,
+    V2Release1660256966235,
+  ],
+  migrationsTransactionMode: "all",
+});
+
+const AppDataSourceDocker = new DataSource({
+  type: "postgres",
   host: "172.21.0.3",
   port: 5432,
   username: "ids_user",
